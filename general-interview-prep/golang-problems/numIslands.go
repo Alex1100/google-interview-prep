a98b39895c97b98cd55a85f129dc4aa334ff8712
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-09-30 14:05:46 -0700

    add number of islands

--- general-interview-prep/golang-problems/numIslands.go
+++ general-interview-prep/golang-problems/numIslands.go
@@ -38,19 +38,121 @@ ________________
   Output: 3
 */
 
+func sinkIsland(grid [][]byte, r, c int) {
+  if grid[r][c] == 1 {
+    grid[r][c] = 0
+  } else {
+    return
+  }
+
+  if r + 1 < len(grid) {
+    sinkIsland(grid, r + 1, c)
+  }
+
+  if r - 1 >= 0 {
+    sinkIsland(grid, r - 1, c)
+  }
+
+  if c + 1 < len(grid[0]) {
+    sinkIsland(grid, r, c + 1)
+  }
+
+  if c - 1 >= 0 {
+    sinkIsland(grid, r, c - 1)
+  }
+}
 
 func numIslands(grid [][]byte) int {
-  fmt.Println(grid[0], grid[0][0])
-  return 1
+  counter := 0
+
+  for i, row := range grid {
+    for j, _ := range row {
+      if grid[i][j] == 1 {
+        counter++
+        sinkIsland(grid, i, j)
+      }
+    }
+  }
+
+  return counter
+}
+
+// LeetCode version
+func sinkIslandLeet(grid [][]byte, r, c int) {
+  if grid[r][c] == 49 {
+    grid[r][c] = 48
+  } else {
+    return
+  }
+
+  if r + 1 < len(grid) {
+    sinkIslandLeet(grid, r + 1, c)
+  }
+
+  if r - 1 >= 0 {
+    sinkIslandLeet(grid, r - 1, c)
+  }
+
+  if c + 1 < len(grid[0]) {
+    sinkIslandLeet(grid, r, c + 1)
+  }
+
+  if c - 1 >= 0 {
+    sinkIslandLeet(grid, r, c - 1)
+  }
+}
+
+func numIslandsLeet(grid [][]byte) int {
+  counter := 0
+
+  for i, row := range grid {
+    for j, _ := range row {
+      if grid[i][j] == 49 {
+        counter++
+        sinkIslandLeet(grid, i, j)
+      }
+    }
+  }
+
+  return counter
 }
 
 func main() {
-  grid := [][]byte{
-    []byte{"1", "1", "1", "1", "0"},
-    []byte{"1", "1", "0", "1", "0"},
-    []byte{"1","1", "0", "0", "0"},
-    []byte{"0", "0", "0", "0", "0"},
+  grid1 := [][]byte{
+    []byte{1, 1, 1, 1, 0},
+    []byte{1, 1, 0, 1, 0},
+    []byte{1, 1, 0, 0, 0},
+    []byte{0, 0, 0, 0, 0},
   }
 
-  fmt.Println("NUMBER OF ISLANDS: ", numIslands(grid))
+  grid2 := [][]byte {
+    []byte{1, 1, 0, 0, 0},
+    []byte{1, 1, 0, 0, 0},
+    []byte{0, 0, 1, 0, 0},
+    []byte{0, 0, 0, 1, 1},
+  }
+
+  // Leet code takes in
+  // an input to the byte array
+  // of a rune and not a string
+  // so 49 is 1 and 48 is 0
+
+  grid3 := [][]byte{
+    []byte{'1', '1', '1', '1', '0'},
+    []byte{'1', '1', '0', '1', '0'},
+    []byte{'1', '1', '0', '0', '0'},
+    []byte{'0', '0', '0', '0', '0'},
+  }
+
+  grid4 := [][]byte {
+    []byte{'1', '1', '0', '0', '0'},
+    []byte{'1', '1', '0', '0', '0'},
+    []byte{'0', '0', '1', '0', '0'},
+    []byte{'0', '0', '0', '1', '1'},
+  }
+
+  fmt.Println("NUMBER OF ISLANDS: ", numIslands(grid1))
+  fmt.Println("NUMBER OF ISLANDS: ", numIslands(grid2))
+  fmt.Println("NUMBER OF ISLANDS: ", numIslandsLeet(grid3))
+  fmt.Println("NUMBER OF ISLANDS: ", numIslandsLeet(grid4))
 }
