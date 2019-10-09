4d54859040ef9e437c2dc15e708b5c546443904e
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-10-08 17:32:33 -0700

    add int stack

--- data-structures/go/stacks/integer_stack.go
+++ data-structures/go/stacks/integer_stack.go
@@ -5,26 +5,50 @@ type IntStack struct {
   Size int
 }
 
-func (s *IntStack) Push(item int) {
+// LIFO
 
+func (s *IntStack) Push(item int) {
+  s.Items = append(s.Items, item)
 }
 
 func (s *IntStack) Pop() int {
-
+  if s.Size > 0 {
+    popped := s.Items[s.Size - 1]
+    s.Items = s.Items[:s.Size - 1]
+    return popped
+  } else {
+    return 0
+  }
 }
 
 func (s *IntStack) Clear() {
-
+  s.Size = 0
+  s.Items = make([]int, 0)
 }
 
 func (s *IntStack) Contains(item int) bool {
+  for _, element := range s.Items {
+    if element == item {
+      return true
+    }
+  }
 
+  return false
 }
 
 func (s *IntStack) Peek() int {
-
+  return s.Items[s.Size - 1]
 }
 
 func (s *IntStack) Clone() *IntStack {
+  clonedStack := &Stack{
+    Items: make([]int, 0),
+    Size: 0,
+  }
 
+  for i := s.Size - 1; i > 0; i-- {
+      clonedStack.Push(s.Pop())
+  }
+
+  return clonedStack
 }
