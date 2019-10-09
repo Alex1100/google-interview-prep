bbcd119fdd9b215cb5659604372fdb391669e328
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-10-09 10:32:38 -0700

    add stack methods

--- data-structures/go/stacks/interface_stack.go
+++ data-structures/go/stacks/interface_stack.go
@@ -5,26 +5,48 @@ type InterfaceStack struct {
   Size int
 }
 
-func (s *InterfaceStack) Push(item int) {
-
+func (s *InterfaceStack) Push(item interface{}) {
+  s.Items = append(s.Items, item)
 }
 
-func (s *InterfaceStack) Pop() int {
-
+func (s *InterfaceStack) Pop() interface{} {
+  if s.Size > 0 {
+    popped := s.Items[s.Size - 1]
+    s.Items = s.Items[:s.Size - 1]
+    return popped
+  } else {
+    return nil
+  }
 }
 
 func (s *InterfaceStack) Clear() {
-
+  s.Size = 0
+  s.Items = make([]interface{}, 0)
 }
 
-func (s *InterfaceStack) Contains(item int) bool {
+func (s *InterfaceStack) Contains(item interface{}) bool {
+  for _, element := range s.Items {
+    if element == item {
+      return true
+    }
+  }
 
+  return false
 }
 
-func (s *InterfaceStack) Peek() int {
-
+func (s *InterfaceStack) Peek() interface{} {
+  return s.Items[s.Size - 1]
 }
 
 func (s *InterfaceStack) Clone() *InterfaceStack {
+  clonedStack := &Stack{
+    Items: make([]interface{}, 0),
+    Size: 0,
+  }
 
+  for i := s.Size - 1; i > 0; i-- {
+      clonedStack.Push(s.Pop())
+  }
+
+  return clonedStack
 }
--- data-structures/go/stacks/string_stack.go
+++ data-structures/go/stacks/string_stack.go
@@ -5,26 +5,48 @@ type StringStack struct {
   Size int
 }
 
-func (s *StringStack) Push(item int) {
-
+func (s *StringStack) Push(item string) {
+  s.Items = append(s.Items, item)
 }
 
-func (s *StringStack) Pop() int {
-
+func (s *StringStack) Pop() string {
+  if s.Size > 0 {
+    popped := s.Items[s.Size - 1]
+    s.Items = s.Items[:s.Size - 1]
+    return popped
+  } else {
+    return ""
+  }
 }
 
 func (s *StringStack) Clear() {
-
+  s.Size = 0
+  s.Items = make([]string, 0)
 }
 
-func (s *StringStack) Contains(item int) bool {
+func (s *StringStack) Contains(item string) bool {
+  for _, element := range s.Items {
+    if element == item {
+      return true
+    }
+  }
 
+  return false
 }
 
-func (s *StringStack) Peek() int {
-
+func (s *StringStack) Peek() string {
+  return s.Items[s.Size - 1]
 }
 
 func (s *StringStack) Clone() *StringStack {
+  clonedStack := &Stack{
+    Items: make([]string, 0),
+    Size: 0,
+  }
 
+  for i := s.Size - 1; i > 0; i-- {
+      clonedStack.Push(s.Pop())
+  }
+
+  return clonedStack
 }
