3d2c7daf614597db963c88a8a0e6abbe6ad0db80
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-10-21 13:04:59 -0700

    add bloom filters

--- data-structures/go/linked_lists/doubly_linked_list.go
+++ data-structures/go/linked_lists/doubly_linked_list.go
@@ -1,47 +1,69 @@
 package linked_lists
 
+// add struct that holds structure
+// so pointers to head and tail are
+// not included on each node
+
 type DoublyIntListNode struct {
   Val int
+  Head *DoublyIntListNode
   Next *DoublyIntListNode
   Prev *DoublyIntListNode
+  Tail *DoublyIntListNode
 }
 
 type DoublyStringListNode struct {
   Val string
+  Head *DoublyStringListNode
   Next *DoublyStringListNode
   Prev *DoublyStringListNode
+  Tail *DoublyStringListNode
 }
 
 type DoublyInterfaceListNode struct {
   Val interface{}
+  Head *DoublyInterfaceListNode
   Next *DoublyInterfaceListNode
   Prev *DoublyInterfaceListNode
+  Tail *DoublyInterfaceListNode
 }
 
 func (ln *DoublyIntListNode) AppendInt(node int) {
   if ln == nil {
     ln = &DoublyIntListNode{
       Val: node,
+      Head: nil,
       Next: nil,
       Prev: nil,
+      Tail: nil
     }
+    ln.Head = ln
+    ln.Tail = ln
   } else {
     var prev *DoublyIntListNode
-
+    head := ln.Head
+    tail := ln.Tail
     curr := ln
+
     for curr.Next != nil {
       prev = curr
       curr = curr.Next
+      curr.Head = head
       curr.Prev = prev
+      curr.Tail = tail
     }
 
     prev = curr
 
     curr.Next = &DoublyIntListNode{
       Val: node,
+      Head: head,
       Next: nil,
       Prev: prev,
+      Tail: nil,
     }
+
+    curr.Next.Tail = curr.Next
   }
 }
 
@@ -49,26 +71,38 @@ func (ln *DoublyStringListNode) AppendString(node string) {
   if ln == nil {
     ln = &DoublyStringListNode{
       Val: node,
+      Head: nil,
       Next: nil,
       Prev: nil,
+      Tail: nil
     }
+    ln.Head = ln
+    ln.Tail = ln
   } else {
     var prev *DoublyStringListNode
-
+    head := ln.Head
+    tail := ln.Tail
     curr := ln
+
     for curr.Next != nil {
       prev = curr
       curr = curr.Next
+      curr.Head = head
       curr.Prev = prev
+      curr.Tail = tail
     }
 
     prev = curr
 
     curr.Next = &DoublyStringListNode{
       Val: node,
+      Head: head,
       Next: nil,
       Prev: prev,
+      Tail: nil,
     }
+
+    curr.Next.Tail = curr.Next
   }
 }
 
@@ -76,29 +110,43 @@ func (ln *DoublyInterfaceListNode) Append(node interface{}) {
   if ln == nil {
     ln = &DoublyInterfaceListNode{
       Val: node,
+      Head: nil,
       Next: nil,
       Prev: nil,
+      Tail: nil
     }
+    ln.Head = ln
+    ln.Tail = ln
   } else {
     var prev *DoublyInterfaceListNode
-
+    head := ln.Head
+    tail := ln.Tail
     curr := ln
+
     for curr.Next != nil {
       prev = curr
       curr = curr.Next
+      curr.Head = head
       curr.Prev = prev
+      curr.Tail = tail
     }
 
     prev = curr
 
     curr.Next = &DoublyInterfaceListNode{
       Val: node,
+      Head: head,
       Next: nil,
       Prev: prev,
+      Tail: nil,
     }
+
+    curr.Next.Tail = curr.Next
   }
 }
 
+
+// fix deletion so it doesn't
 func (ln *DoublyIntListNode) DeleteInt(node int) (int, *DoublyIntListNode) {
   if ln == nil {
     return -1, ln
@@ -131,8 +179,10 @@ func (ln *DoublyIntListNode) DeleteInt(node int) (int, *DoublyIntListNode) {
   if node == curr.Val {
     toDelete = curr
     temp := curr.Prev
+    temp.Tail
     curr.Prev.Next = nil
     curr.Prev.Prev.Next = temp
+
   }
 
   return toDelete.Val, head
